a83ef13c2810a71499c84eea3970a13b9a997c43
DESKTOP-FICELG4\knigh <dogukanacet@gmail.com>
2023-09-09 11:35:16 +0300

    sidebar progress

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import '@styles/globals.css'
 import { Inter } from 'next/font/google'
-import Sidepanel from '@components/Sidepanel'
+import Sidebar from '@components/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,13 +11,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html>
+      <head />
       <body>
-        <main className='app'>
-          <Sidepanel />
+        <Sidebar />
+        <div className="content">
           {children}
-        </main>
-    </body>
-  </html>
+        </div>
+      </body>
+    </html>
   )
 }
---
+++ components/Sidebar.js
@@ -0,0 +1,41 @@
+"use client"
+
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+const Sidebar = () => {
+  const navLinks = [
+    {
+      label: "Home",
+      href: "/",
+      iconPath: "/assets/home-icon.svg",
+    },
+    {
+      label: "Comments",
+      href: "/comments",
+      iconPath: "/assets/comments-icon.svg",
+    },
+  ];
+  return (
+    <div className="sidebar">
+      <Link className="title" href={"/"}>
+        NextJS 13
+      </Link>
+      {navLinks.map((link) => {
+        const isActive = usePathname() === link.href;
+        return (
+          <Link
+            className={`link-item${isActive ? " active" : ""}`}
+            href={link.href}
+          >
+            <Image src={link.iconPath} alt="icon" width={30} height={30} />
+            {link.label}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Sidebar;
